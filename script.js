57264d6faa1d59038c1aad44f8df4d5cd426cf08
VladimirTi <tytmes@mail.ru>
2019-05-23 21:04:45 +0300

    ball variable added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const ball = document.querySelector('#ball')
 document.querySelector('#field').addEventListener('click', (event) => {
   [ball.style.left, ball.style.top] = [`${Math.min(180, Math.max(event.offsetX, 20))}px`, `${Math.min(130, Math.max(event.offsetY, 20))}px`];
 });
